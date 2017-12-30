a6810a4d8a8a277d164e67118a3054e025b79b41
CodeDad <github email address>
2017-12-30 15:08:38 -0500

    fixed homepage to show a create card link if a child doesn't have one

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,8 +5,12 @@ class Child < ApplicationRecord
 	has_many :cards
 	accepts_nested_attributes_for :cards
 
+	# def has_card
+	# 	self.age != nil
+	# 	self.height != nil
+	# 	self.weight !=nil
+	# end
 	
-
 	def latest_card
 		oldest = self.cards.maximum("age")
 		self.cards.find_by(age: (oldest))
