2a062b980cffcc1985f2f28e0d545aff7b9b1879
CodeDad <github email address>
2018-01-08 15:12:07 -0500

    added helper methods to child. Profile fields for eye color, hair color and gender show up in the profile_view only if filled out. Blank fields do not show

--- app/helpers/children_helper.rb
+++ app/helpers/children_helper.rb
@@ -1,30 +1,25 @@
 module ChildrenHelper
 
-		def fix_date(child)
-			child.birthday.strftime("%B %e, %Y")
+	def fix_date(child)
+		child.birthday.strftime("%B %e, %Y")
+	end
+
+	def eye_check(child)
+		if child.eye_color != ""
+			"Eye Color: #{child.eye_color}"
 		end
+	end
 
+	def hair_check(child)
+		if child.hair_color != ""
+			"Hair Color: #{child.hair_color}"
+		end
+	end
 
-		# def year_check
-		# 	self.cards.detect do |check|
-		# 		check.years != 0
-		# 	end
-		# end
-
-		
-		# def latest_card
-		# 	if year_check
-		# 		oldest = self.cards.maximum("years")
-		# 		self.cards.find_by(years: (oldest))
-		# 	else 
-		# 		oldest = self.cards.maximum("months")
-		# 		self.cards.find_by(months: (oldest))
-		# 	end
-		# end
-
-		# def ordered
-		# 	self.cards.order("years ASC", "months ASC")
-		# end
-
+	def gender_check(child)
+		if child.gender != ""
+			"Gender: #{child.gender}"
+		end
+	end
 
 end
