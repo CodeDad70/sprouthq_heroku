50cad4deae78fd68e147b02f7cf58de36ada86ce
CodeDad <github email address>
2018-01-21 23:22:09 -0500

    refactored latest_card method more to remove if else statement since it is now not needed

--- app/models/child.rb
+++ app/models/child.rb
@@ -12,23 +12,10 @@ class Child < ApplicationRecord
 	validates_length_of :hair_color, :minimum => 1, :maximum => 15, :allow_blank => true
 	validates_length_of :gender, :minimum => 1, :maximum => 15, :allow_blank => true
 
-	def year_check
-		self.cards.detect do |check|
-			check.years != 0
-		end
-	end
-
 	def latest_card
-		if year_check
 			oldest = self.ordered.last
-			
 			self.cards.find_by(id: (oldest.id))
-		else 
-			oldest = self.cards.maximum("months")
-			self.cards.find_by(months: (oldest))
-		end
 	end
-
 
 	def ordered
 		self.cards.order("years ASC", "months ASC")
