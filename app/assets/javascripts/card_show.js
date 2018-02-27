92fc5ac8ba70400c88f4b8be8e8b66b101373161
CodeDad <github email address>
2018-02-26 23:33:35 -0500

    added a .div - childId to the partial that is tied to the child's id passed through json. will use that to assign the updates to the proper div

--- app/assets/javascripts/card_show.js
+++ app/assets/javascripts/card_show.js
@@ -5,21 +5,24 @@ $(document).ready(selectCard)
   function selectCard(card_number){
 		$("#age a ").click(function(e){
     	e.preventDefault();
-    	console.log(this.innerHTML)
     	cardSelect = this.href
     	findCard(cardSelect)
   	});
 	};
 
 	function findCard(cardSelect){
-		 $.get(cardSelect +".json", function(data){	
-		 $(".age").text(data["age"])
-		 $(".height").text(data["height"])
-		 $(".weight").text(data["weight"])
-		 $(".advil").text(data["advil"])
-		 $(".tylenol").text(data["tylenol"])
-		 $(".flu").text(data["flu"])
+		
+		 $.get(cardSelect +".json", function(data){
+		 	console.log(data.child["id"])
+		 // if (data[#childId] === this.child[id])	{
+		 // $(".age").text(data["age"])
+		 // $(".height").text(data["height"])
+		 // $(".weight").text(data["weight"])
+		 // $(".advil").text(data["advil"])
+		 // $(".tylenol").text(data["tylenol"])
+		 // $(".flu").text(data["flu"])
 		 // $(".field").text(data["image"])
+		// }
 		})
 	}	
 
