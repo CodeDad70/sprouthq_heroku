56eb6ef9761d1b668e1d86bdadcb79cbf1236019
CodeDad <github email address>
2018-02-28 15:44:58 -0500

    added a quick jump link to profile section of card to enable user to jump to a different child's profile

--- app/assets/javascripts/card_show.js
+++ app/assets/javascripts/card_show.js
@@ -44,9 +44,8 @@ $(document).ready(selectCard)
     	console.log(data.child.name)
     		$('.stones').append("<small>" + milestone.title + "</small> <br>")
     
-	})
-		
-	})
+			})
+		})
 	};	
 
 
